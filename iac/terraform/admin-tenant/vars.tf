569657fa8d3796af7796016097ef387d0ff4a298
jeeva-duplo <50436466+jeeva-duplo@users.noreply.github.com>
2024-02-19 23:36:09 +0530

    Update vars.tf

--- iac/terraform/admin-tenant/vars.tf
+++ iac/terraform/admin-tenant/vars.tf
@@ -2,5 +2,5 @@
 variable "infra_name" {
   type    = string
   description = "The DuploCloud Infrastructure that the DuploCloud Tenant will be added to"
-  default = "prod-infra"
+  default = "nonprod"
 }
